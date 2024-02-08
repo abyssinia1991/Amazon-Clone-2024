bb6ba72a5e9a0000c74e84ae6c6bb4d8feeb2855
Abyssinia <abyssinia1991@gmail.com>
2024-02-08 18:52:47 -0500

    add the new fil carouse

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,14 @@
+import "./App.css";
+import React from "react";
 
-import './App.css';
-import Header from './Components/Header/Header';
+import CarouselEffect from "./Components/Carousel/CarouselEffect";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <CarouselEffect />
     </div>
   );
 }
