1aea5fd19265f3745327843b0b41036a89004e33
Abyssinia <abyssinia1991@gmail.com>
2024-03-04 21:07:13 -0500

    adding the productDetail

--- amazon-clone/src/Components/Product/ProductCard.js
+++ amazon-clone/src/Components/Product/ProductCard.js
@@ -59,40 +59,4 @@ export default ProductCard;
 
 
 
-// import React from "react";
-// import Rating from "@mui/material/Rating";
-// import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
-// import classes from "./Product.module.css"
-// import { Link}  from "react-router-dom";
 
-// function ProductCard({ Product }) {
-//   const { image, title, id, rating, price } = Product;
-  
-//   return (
-//     <div className={`${classes.card_container}`}>
-//       <Link to={`/product/${id}`} >
-//         <img src={image} alt="" />
-//       </Link>
-//       <div>
-//         <h3>{title}</h3>
-//         <div className={classes.rating}>
-//           {/* rating */}
-//           <Rating value={rating.rate} precision={0.1} />
-//           {/* count */}
-//           <small>{rating.count}</small>
-//         </div>
-//         <div>
-//           {/* price */}
-
-//           <CurrencyFormat amount={price} />
-//         </div>
-//               <button className={classes.button}>
-//                   Add to Cart
-                  
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ProductCard;
